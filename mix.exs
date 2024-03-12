8815363e6795c20ea2b42278bff6213498bc0eae
Andy Kirkpatrick <ubermonk@gmail.com>
2024-03-12 01:59:42 +0000

    Upate deps

--- mix.exs
+++ mix.exs
@@ -5,27 +5,34 @@ defmodule Kword.MixProject do
     [
       app: :kword,
       version: "0.1.0",
-      description: "A library of keyword-list handling functions to complement Keyword",
-      licenses: ["Apache 2.0"],
       elixir: ">= 1.15.0",
+      description: "A library of keyword-list handling functions to complement Keyword",
+      package: package(),
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      source_url: "https://github.com/spacebat/kword"
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
-    [
-      extra_applications: [:logger]
-    ]
+    []
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ex_doc, github: "elixir-lang/ex_doc", only: [:dev, :test], runtime: false},
-      {:ex_check, "~> 0.12", only: [:dev, :test]},
-      {:credo, ">= 0.0.0", only: [:dev, :test], runtime: false}
+      {:ex_doc, "~> 0.31", only: [:dev], runtime: false},
+      {:ex_check, "~> 0.16", only: [:dev, :test], runtime: false},
+      {:credo, "~> 1.7.5", only: [:dev, :test], runtime: false}
+    ]
+  end
+
+  def package() do
+    [
+      files: ~w(lib priv .formatter.exs mix.exs README* readme* LICENSE* license* CHANGELOG* changelog* src),
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/elixir-ecto/postgrex"}
     ]
   end
 end
